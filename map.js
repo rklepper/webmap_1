d67351f9d117aad050b2f5265ba30e49b2200c03
rklepper <rtk2119@tc.columbia.edu>
2020-03-06 12:08:22 -0500

    Update map.js
    added mapbox token

--- map.js
+++ map.js
@@ -1,1 +1,7 @@
-console.log("Hello, world!")
+'use strict'        // let the browser know we're serious
+
+// debug statement letting us know the file is loaded
+console.log('Loaded map.js')
+
+// your mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoicmtsZXBwZXIiLCJhIjoiY2s3Z2ZsYmdhMGQ2ZTNmb2RjbDBuanB4diJ9.sSYh3Y87xtJWWc4xSzyBZA'
